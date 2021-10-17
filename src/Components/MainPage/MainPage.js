f4bac2f3c5bc331cab05901cd5ff80c3efb8d56d
Andrzej Smoliński <58042259+AnterroZZ@users.noreply.github.com>
2021-10-17 13:39:22 +0200

    Applied base drawing ability

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,164 +1,69 @@
 import Node from "./Nodes/Node";
 import styles from "./MainPage.module.css";
+import { useEffect, useState } from "react";
+
+// Node types are: clear, wall, start, end, visited
+function SingleNode(id, row, column) {
+  this.id = id;
+  this.row = row;
+  this.column = column;
+  this.type = "clear";
+  this.isStart = false;
+  this.isEnd = false;
+  this.isWall = false;
+  this.isVisited = false;
+}
+
+const populateNodes = () => {
+  let nodes = [];
+  for (let row = 0; row < 20; row++) {
+    let currentRow = [];
+    for (let column = 0; column < 50; column++) {
+      currentRow.push(new SingleNode(row * 50 + column, row, column));
+    }
+    nodes.push(currentRow);
+  }
+
+  return nodes;
+};
+
 const MainPage = () => {
+  const [isPopulated, setIsPopulated] = useState(false);
+  const [nodes, setNodes] = useState([]);
+  const [isInDrawingMode, setIsInDrawingMode] = useState(false);
+  const [drawingType, setDrawingType] = useState("");
+
+  useEffect(() => {
+    let nodes = populateNodes();
+    console.log("Populating nodes");
+    setNodes(nodes);
+    setIsPopulated(true);
+  }, []);
+
   return (
-    <div className={styles.nodesWraper}>
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
-      <Node />
+    <div
+      onMouseLeave={() => setIsInDrawingMode(false)}
+      className={styles.nodesWraper}
+    >
+      {isPopulated &&
+        nodes.map((row, rowIdx) => {
+          return (
+            <div className={styles.rowWraper} key={rowIdx}>
+              {row.map((node, nodeidx) => {
+                return (
+                  <Node
+                    key={node.id}
+                    isInDrawingMode={isInDrawingMode}
+                    setIsInDrawingMode={setIsInDrawingMode}
+                    drawingType={drawingType}
+                    setDrawingType={setDrawingType}
+                    node={node}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
     </div>
   );
 };
--- src/Components/MainPage/Nodes/Node.js
+++ src/Components/MainPage/Nodes/Node.js
@@ -1,7 +1,66 @@
+import { useState } from "react";
 import styles from "./Node.module.css";
 
-const Node = () => {
-  return <div className={styles.node}></div>;
+const Node = ({
+  node,
+  isInDrawingMode,
+  setIsInDrawingMode,
+  setDrawingType,
+  drawingType,
+}) => {
+  const [singleNode, setSingleNode] = useState(node);
+  const handleOnHover = () => {
+    if (isInDrawingMode) {
+      switch (drawingType) {
+        case "wall":
+          if (singleNode["type"] === "clear") {
+            setSingleNode({ ...singleNode, type: "wall" });
+          }
+          break;
+        case "clear":
+          if (singleNode["type"] === "wall") {
+            setSingleNode({ ...singleNode, type: "clear" });
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const handleStartDrawing = (event) => {
+    event.preventDefault();
+    setIsInDrawingMode(true);
+    switch (singleNode.type) {
+      case "clear":
+        setDrawingType("wall");
+        setSingleNode({ ...singleNode, type: "wall" });
+
+        console.log("Drawing type set to wall");
+        break;
+      case "wall":
+        console.log("Drawing type set to clear");
+        setSingleNode({ ...singleNode, type: "clear" });
+        setDrawingType("clear");
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleStopDrawing = () => {
+    setIsInDrawingMode(false);
+  };
+  return (
+    <div
+      onMouseDown={handleStartDrawing}
+      onMouseUp={handleStopDrawing}
+      onMouseOver={handleOnHover}
+      className={`${styles.node} ${
+        singleNode.type === "wall" ? styles.wall : ""
+      }`}
+    ></div>
+  );
 };
 
 export default Node;
