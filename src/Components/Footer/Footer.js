499ca2c6ba6957ebd8e448b0bc94e2582891e45a
Andrzej Smolinski <anterroz@gmail.com>
2021-10-17 02:39:27 +0200

    First implementation of footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,12 @@
+import styles from "./Footer.module.css";
+
 const Footer = () => {
-  return <div></div>
-}
+  return (
+    <div className={styles.wraper}>
+      <p>Statistics</p>
+      <p>Compare algo</p>
+    </div>
+  );
+};
 
 export default Footer;
